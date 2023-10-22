0e650d1426dcd5b4564c79c45f72dda178ad0ea2
xionkq <3030199850@qq.com>
2023-10-22 22:30:04 +0800

    try to require sakana failed :(

--- src/script/preload.js
+++ src/script/preload.js
@@ -1,5 +1,1 @@
 const { contextBridge, ipcRenderer } = require('electron')
-const Sakana = require('../../node_modules/sakana')
-
-// contextBridge.exposeInMainWorld('sakana', Sakana)
-// console.log(Sakana)
--- src/script/renderer.js
+++ src/script/renderer.js
@@ -1,4 +1,4 @@
-window.sakana.init({
+Sakana.init({
   el: '.sakana-box',     // 启动元素 node 或 选择器
   scale: .5,                // 缩放倍数
   canSwitchCharacter: true,      // 允许换角色
