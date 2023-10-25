d0be9cb11f75804f60fb0e0d8ad5525f2145331c
xionkq <3030199850@qq.com>
2023-10-25 22:35:42 +0800

    use ts

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,16 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'script/preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
     },
   })
-
+  // win.loadFile('src/index.html')
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    win.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/src/index.html`));
+  }
   win.webContents.openDevTools()
-  win.loadFile('src/index.html')
 }
 
 app.whenReady().then(() => {
