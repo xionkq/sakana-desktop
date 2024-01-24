3a243d7d8dbfa6ba59503f1356e86564af14a89c
xionkq <3030199850@qq.com>
2024-01-24 21:02:49 +0800

    use vue3

--- src/scripts/renderer.ts
+++ src/scripts/renderer.ts
@@ -1,5 +1,4 @@
-Sakana.init({
-  el: '.sakana-box',     // 启动元素 node 或 选择器
-  scale: .5,                // 缩放倍数
-  canSwitchCharacter: true,      // 允许换角色
-});
+import { createApp } from 'vue';
+import App from '../App.vue';
+
+createApp(App).mount('#app');
