30a0441b4f4ad9b89dadd862476c35e182396890
xionkq <3030199850@qq.com>
2024-02-20 21:46:17 +0800

    use mouse penetrate

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
-<script setup>
+<script setup lang="ts">
 import Sakana from './components/Sakana.vue'
 import Toolbar from './components/Toolbar.vue'
-import {onMounted, ref} from 'vue'
+import {computed, onMounted, ref} from 'vue'
 
 const sakanaRef = ref(null)
+const isSakanaClicked = ref(false)
+
+const NOT_ALLOW_PENETRATE_LIST = ['sakana-character', 'toolbar']
+
+function isNotAllowPenetrate(classList: DOMTokenList) {
+  const list = Array.from(classList)
+  return list.some((item) => NOT_ALLOW_PENETRATE_LIST.includes(item))
+}
 
 onMounted(() => {
   window.addEventListener("mousemove", event => {
-    // TODO: Use custom attribute judgment
-    if (event.target?.classList.contains('sakana-character')){
+    if (isSakanaClicked.value || isNotAllowPenetrate((event.target as Element).classList)){
       window.winPenetrateFalse()
     }
     else {
       window.winPenetrateTrue()
     }
   });
+
+  const sakanaCharacter = document.getElementsByClassName('sakana-character')[0]
+  sakanaCharacter.addEventListener('mousedown', () => {
+    isSakanaClicked.value = true
+  })
+  window.addEventListener('mouseup', () => {
+    isSakanaClicked.value === true && (isSakanaClicked.value = false)
+  })
 })
 
 function checkout() {
